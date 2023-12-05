0e64b2d3a34470ef3bcc3dd8eb5aab6a6b910a80
Ostap4ello <okpeleh@gmail.com>
2023-12-05 10:45:10 +0100

    Modified README.md and dublicated it to the code

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-# ludo_test
-Trying to write ludo game
-end of readme.md
+...instead of readme.md 
 
-I wrote this line on laptop
+### LUDO - Človeče nehnevaj sa ###
+by Ostap Pelekh, 1bc., API7
+
+Program simulates The LUDO game.
+It begins with two prompts that ask you about size of board and number of players.
+After that it shows step-by-step simulation of game.
+# !!! Please, read the prompts shown in the game !!! #
+
+# Differences:
+1. Next figure of player goes on board only after previous finishes (so each player have max. 1 figure on the board)
+2. When two players are on the same field, they are displayed, as both of letters (for example, when 'A' meets 'B', they are shown as 'AB')
+=> Rules with 'blocking the field' and 'meeting on one field' are not implemented
+ 
+ 
+ ... end of readme.md
--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+# ...instead of readme.md 
+# 
+# LUDO - Človeče nehnevaj sa 
+# by Ostap Pelekh, 1bc., API7
+# 
+# Program simulates The LUDO game.
+# It begins with two prompts that ask you about size of board and number of players.
+# After that it shows step-by-step simulation of game.
+# !!! Please, read the prompts game shows !!!
+#
+# Differences:
+# 1. Next figure of player goes on board only after previous finishes (so each player have max. 1 figure on the board)
+# 2. When two players are on the same field, they are displayed, as both of letters (for example, when 'A' meets 'B', they are shown as 'AB')
+# => Rules with 'blocking the field' and 'meeting on one field' are not implemented
+#  
+#  ... end of readme.md
+
 from random import randint
 from os import system, name
 
@@ -160,7 +177,7 @@ def main(): #MAIN BODY
         except:
             print('Please write a number!')
         else:
-            if sizeOfBoard >= 4:
+            if sizeOfBoard > 4:
                 break
             else:
                 print('Please write a number bigger than 4!')
